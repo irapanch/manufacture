75d130fc09e6b914a68fd2a3ba3eb5f201068dac
irapanch <117355580+irapanch@users.noreply.github.com>
2024-12-30 21:07:07 +0200

    team template

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,43 @@
-const navMenu = document.querySelector(".header__menu")
-const menuMobile = document.querySelector(".menu__list-mobile")
-const menuBtn = document.querySelector(".header-mobile-menu-btn")
+// const navMenu = document.querySelector(".header__menu")
+// const menuMobile = document.querySelector(".menu__list-mobile")
+// const menuBtn = document.querySelector(".header-mobile-menu-btn")
 
-menuBtn.addEventListener('click', () => {
+// menuBtn.addEventListener('click', () => {
 
+//     navMenu.classList.toggle('menu--open');
+// });
+
+// menuMobile.addEventListener('click', () => {
+
+//     navMenu.classList.toggle('menu--open');
+// });
+
+
+
+
+
+const navMenu = document.querySelector(".header__menu");
+const menuBtn = document.querySelector(".header-mobile-menu-btn");
+const menuCloseBtn = document.querySelector(".header-menu-btn-close");
+const mainSection = document.querySelector("main");
+
+// Обробник відкриття меню
+menuBtn.addEventListener('pointerdown', () => {
     navMenu.classList.toggle('menu--open');
+    // Додати обробник для закриття меню поза межами
+    mainSection.addEventListener('pointerdown', closeMenuOnPointerDownOutside);
 });
 
-menuMobile.addEventListener('click', () => {
-
-    navMenu.classList.toggle('menu--open');
+// Закриття меню через кнопку закриття
+menuCloseBtn.addEventListener('pointerdown', () => {
+    navMenu.classList.remove('menu--open');
 });
+
+// Закриття меню, якщо натискається поза меню
+function closeMenuOnPointerDownOutside(event) {
+    // Перевіряємо, чи натискається поза межами меню
+    if (!navMenu.contains(event.target) && !menuBtn.contains(event.target)) {
+        navMenu.classList.remove('menu--open');
+        mainSection.removeEventListener('pointerdown', closeMenuOnPointerDownOutside); // Видаляємо слухача після закриття
+    }
+}

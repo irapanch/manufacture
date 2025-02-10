4e815028b5b3893b2dcdccbf3a31d0833acaafa3
irapanch <117355580+irapanch@users.noreply.github.com>
2025-02-10 15:02:19 +0200

    reviews adaptive

--- js/swipers.js
+++ js/swipers.js
@@ -1,24 +1,35 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
 
-const swiperReviews = new Swiper('.swiper-reviews', {
-   
-    pagination: {
-      el: '.swiper-pagination-reviews',
-      clickable: true,
-    },
-   
-   
-  });
+const swiperReviews = new Swiper(".swiper-reviews", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination-reviews",
+    clickable: true,
+  },
+});
 
-  const swiperSertificates = new Swiper('.swiper-sertificates', {
-    slidesPerView: 2,
-      centeredSlides: false,
+const swiperSertificates = new Swiper(".swiper-sertificates", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination-sertificates",
+    clickable: true,
+  },
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
       spaceBetween: 20,
-    pagination: {
-      el: ".swiper-pagination-sertificates",
-      clickable: true,
+      centeredSlides: false,
+      watchOverflow: true,
+      loop: true,
     },
-   
-    watchOverflow: true,
-  });
-
+   768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      centeredSlides: false,
+      watchOverflow: true,
+      loop: true,
+    },
+  },
+});

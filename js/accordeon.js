f334052c4f88b95e41dda4459c6528c7f4dfb221
irapanch <117355580+irapanch@users.noreply.github.com>
2025-02-11 16:48:00 +0200

    faq adaptive

---
+++ js/accordeon.js
@@ -0,0 +1,16 @@
+const accordeonTitles = document.querySelectorAll('.accordeon-title')
+
+accordeonTitles.forEach( function(accordeonTitle){
+    accordeonTitle.addEventListener('click', function(){
+        const currentText = accordeonTitle.parentElement.querySelector('.accordeon-item-answers')
+
+        accordeonTitle.classList.toggle('accordeon-title--active')
+        currentText.classList.toggle('accordeon-item-answers--visible')
+
+        if (currentText.classList.contains('accordeon-item-answers--visible')) {
+          currentText.style.maxHeight = currentText.scrollHeight + 'px'  
+        } else {
+            currentText.style.maxHeight = null
+        }
+    })
+})
